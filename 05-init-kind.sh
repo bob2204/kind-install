bdd76fdccf89dc9ba8639c95a9548b51b95f38b4
Bruno Guérin <bguerin@dawan.fr>
2024-02-22 14:57:59 +0100

    [UPDATE] Variabilisation de l'IP de l'APIServer

--- 05-init-kind.sh
+++ 05-init-kind.sh
@@ -1,13 +1,16 @@
 #!/bin/bash
 #
 # 2023-12-03
+# 2024-02-22
 #
 
 # Initialisation du clusteur 'stage'
 
 curl -Lo ./kind https://kind.sigs.k8s.io/dl/v0.20.0/kind-linux-amd64
 chmod +x ./kind
-./kind create cluster --name=stage --config=kcluster.yml
+
+export IPV4=$(ip -f inet address show enp0s8|grep -Po 'inet \K[\d.]+')
+./kind create cluster --name=stage --config=<(envsubst < kcluster.yml)
 
 # Attendre un peu...
 
