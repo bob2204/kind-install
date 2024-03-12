adab52427dedf7307a4ae2d214f336655d37ad27
Bruno Guérin <bguerin@dawan.fr>
2024-03-12 10:02:32 +0100

    [NEW] Helm Completion

--- 04-config-kubectl-helm.sh
+++ 04-config-kubectl-helm.sh
@@ -7,3 +7,5 @@
 echo 'source <(kubectl completion bash)' >>~/.bashrc
 echo 'alias k=kubectl' >>~/.bashrc
 echo 'complete -o default -F __start_kubectl k' >>~/.bashrc
+
+echo 'source <(helm completion bash)' >>~/.bashrc
