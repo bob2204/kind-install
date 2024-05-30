b1c636c04c16e37569fb2bc9605788ff3dea22f6
Bruno Guérin <bguerin@dawan.fr>
2024-05-30 13:29:17 +0200

    [UPDATE] Kubernetes Version

--- 02-install-kubectl.sh
+++ 02-install-kubectl.sh
@@ -5,10 +5,12 @@
 # NB: À faire en tant que root !
 #
 
+source env.conf
+
 # Installation de kubectl
 
 apt-get update && apt-get install -y apt-transport-https curl gnupg2
-curl -fsSL https://pkgs.k8s.io/core:/stable:/v1.28/deb/Release.key | gpg --dearmor -o /usr/share/keyrings/kubernetes-apt-keyring.gpg
-echo 'deb [signed-by=/usr/share/keyrings/kubernetes-apt-keyring.gpg] https://pkgs.k8s.io/core:/stable:/v1.28/deb/ /' > /etc/apt/sources.list.d/kubernetes.list
+curl -fsSL https://pkgs.k8s.io/core:/stable:/${KUBE_VERSION}/deb/Release.key | gpg --dearmor -o /usr/share/keyrings/kubernetes-apt-keyring.gpg
+echo "deb [signed-by=/usr/share/keyrings/kubernetes-apt-keyring.gpg] https://pkgs.k8s.io/core:/stable:/${KUBE_VERSION}/deb/ /" > /etc/apt/sources.list.d/kubernetes.list
 apt-get update
 apt-get install -y kubectl
