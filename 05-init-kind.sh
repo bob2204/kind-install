ad07c7ffdd5d53088f99cc688eeb99bd8761012c
Bruno Guérin <bguerin@dawan.fr>
2024-12-07 14:52:13 +0100

    [UPDATE] Ajout du Kubelet Serving Certificate Approver

--- 05-init-kind.sh
+++ 05-init-kind.sh
@@ -19,6 +19,10 @@ chmod +x ./kind
 export IPV4=$(ip -f inet address show ${KIND_IFACE}|grep -Po 'inet \K[\d.]+')
 ./kind create cluster --name=${KIND_CLUSTER_NAME} --config=<(envsubst < kcluster.yml)
 
+# Installation du Kubelet Serving Certificate Approver
+
+kubectl apply -f https://raw.githubusercontent.com/alex1989hu/kubelet-serving-cert-approver/main/deploy/standalone-install.yaml
+
 # Attendre un peu...
 
 echo "Attendre un peu (30s)"
@@ -27,3 +31,4 @@ sleep 30
 # Installation du CNI *Calico*
 
 kubectl apply -f calico.yml
+
--- data/data.md
+++
@@ -1,3 +0,0 @@
-***Infos***
-* Répertoire en montage *bind* sur l'ensemble des nodes.
-* Peut être utilisé en volume *hostPath.
