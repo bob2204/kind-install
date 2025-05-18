26c72d8e88667fb333bc4d5b22a1431addf38d90
Bruno Guérin <bguerin@dawan.fr>
2025-05-18 18:54:33 +0200

    [UPDATE] Kind-v0.28.0 - Kind-cloud-provider-v0.6.0

--- scripts.d/03-install-kind-cloud-provider.sh
+++ scripts.d/03-install-kind-cloud-provider.sh
@@ -4,9 +4,10 @@
 #
 
 source env.conf
-TARFILE=cloud-provider-kind_0.2.0_linux_amd64.tar.gz
-wget https://github.com/kubernetes-sigs/cloud-provider-kind/releases/download/v0.2.0/${TARFILE}
-tar xf ${TARFILE}
+CLOUD_PROVIDER_VERSION=0.6.0
+TARFILE=cloud-provider-kind_${CLOUD_PROVIDER_VERSION}_linux_amd64.tar.gz
+wget https://github.com/kubernetes-sigs/cloud-provider-kind/releases/download/v${CLOUD_PROVIDER_VERSION}/${TARFILE}
+tar xf ${TARFILE} cloud-provider-kind
 mv cloud-provider-kind /usr/local/bin
 rm ${TARFILE}
 
